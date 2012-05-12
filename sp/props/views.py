0bb50fef9902e7af8514a82e27fb380d4ba47bce
Pat <patrickjamesbrown@gmail.com>
2012-05-12 12:31:42 +1000

    All diff functions submitting to database properly.

--- sp/props/views.py
+++ sp/props/views.py
@@ -7,12 +7,16 @@ from sp.microcons.models import MicroCons
 from sp.props.diff_match_patch import *
 
 def edit_article(request, articleid):
+	
+	# form structure
+	
 	errors = []
 
 	if request.method == 'POST':
 		dfunction = diff_match_patch()
 				
-		# formatinitialtext
+		# Creating version to be amended to run diff against.
+		
 		first = MicroCons.objects.filter(id__contains=articleid).values()
 		valuelist = first[0]
 		data = valuelist['articlecontent']
@@ -21,22 +25,31 @@ def edit_article(request, articleid):
 		form = PropForm(request.POST)
 		if form.is_valid():
 			
+			# Running diff functions.
+			
 			data = form.cleaned_data['article']
 			asciidata = data.encode("utf8")
 			diff = dfunction.diff_main(formatted, asciidata)
 			diffhtml = dfunction.diff_prettyHtml(diff)
-			p = Props(idversions=articleid, maindiff=diff, patch=diff, htmldiff=diffhtml)
+			
+			patchdata = dfunction.patch_make(formatted, asciidata)
+			# Saving diff results to database.
+			
+			p = Props(idversions=articleid, maindiff=diff, patch=patchdata, htmldiff=diffhtml)
 			p.save()
 		
 		return render_to_response('donediff.html', {'diff': diffhtml})
 
 	else:
 		
+		# Put together initial data for the form.
+		
 		first = MicroCons.objects.filter(id__contains=articleid).values()
 		valuelist = first[0]
 		data = valuelist['articlecontent']
 		initial = data.encode("utf8")
 		
+		# Displaying initial data in form.
 		
 		populate = MicroCons.objects.get(id__contains=articleid)
 		form = PropForm(initial={'article': initial})
