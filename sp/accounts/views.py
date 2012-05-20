b8e344d72c1fc99aa7a9e84b1c3b2700175e4a92
Pat <patrickjamesbrown@gmail.com>
2012-05-20 18:50:30 +1000

    Half way through the standard user system being implemented.

--- sp/accounts/views.py
+++ sp/accounts/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 
-def home(request):
-	return render_to_response('home_page.html')
+def login_page(request):
+	return render_to_response('login.html')	
 
 def register(request):
     if request.method == 'POST':
@@ -19,18 +19,21 @@ def register(request):
         'form': form,
     })
 
-def login_view(request):
-    username = request.POST.get('username', '')
-    password = request.POST.get('password', '')
-    user = auth.authenticate(username=username, password=password)
-    if user is not None and user.is_active:
-        # Correct password, and the user is marked "active"
-        auth.login(request, user)
-        # Redirect to a success page.
-        return HttpResponseRedirect("/account/loggedin/")
-    else:
-        # Show an error page
-        return HttpResponseRedirect("/account/invalid/")# Create your views here.
+# def login_view(request):
+#     username = request.POST.get('username', '')
+#     password = request.POST.get('password', '')
+#     user = auth.authenticate(username=username, password=password)
+#     if user is not None and user.is_active:
+#         # Correct password, and the user is marked "active"
+#         auth.login(request, user)
+#         # Redirect to a success page.
+#         return HttpResponseRedirect("/login_page/")
+#     else:
+#         # Show an error page
+#         return HttpResponseRedirect("/user_home/")
 
 def register_done(request):
 	return render_to_response('register_done.html')
+	
+def user_home(request):
+	return render_to_response('user_home.html', {'user':user})
--- sp/urls.py
+++ sp/urls.py
@@ -3,18 +3,23 @@ from sp.views import insta_links
 from django.contrib import admin
 admin.autodiscover()
 from sp.microcons.views import micro_cons, micro_done
+from django.contrib.auth.views import login, logout
 from sp.article.views import latest_articles, view_article
 from sp.props.views import create_prop, view_article_props, view_latest_props, view_single_prop, prop_accept
-from sp.accounts.views import register, register_done, home
+from sp.accounts.views import register, register_done, login_page, user_home
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+	(r'^accounts/login/$', login),
+    (r'^accounts/logout/$', logout),
 	(r'^register/$', register),
 	(r'^register_done/$', register_done),
-	(r'^$', home),
+	# (r'^$', login_view),
+	(r'^login_page/$', login_page),
+	(r'^user_home/$', user_home),
 	(r'^insta_links/$', insta_links),
 	(r'^admin/', include(admin.site.urls)),
 	(r'^micro_constitution/$', micro_cons),
