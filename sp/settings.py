9419d3dc601c3a52bbf3a7cffbc4c903cbbaa764
Pat <patrickjamesbrown@gmail.com>
2012-05-20 11:42:57 +1000

    Accounts app added.

--- sp/settings.py
+++ sp/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'sp.accounts',
 	'sp.microcons',
 	'sp.article',
 	'sp.props',
