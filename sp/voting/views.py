d9296f684e6a0e0ed4063135c2f858ccfc96ea91
Pat <patrickjamesbrown@gmail.com>
2012-05-26 21:40:09 +1000

    Upvote and downvote operating.

--- sp/voting/views.py
+++ sp/voting/views.py
@@ -5,18 +5,37 @@ from django.template import loader, RequestContext
 from sp.props.models import Props
 from sp.props.forms import PropForm
 from sp.microcons.models import MicroCons
+from sp.voting.models import Vote
 from sp.props.diff_match_patch import *
 from django.contrib.auth.decorators import login_required
 
 def up_vote(request, propid):
 	
 	count = Vote.objects.filter(prop_id__contains=propid).values()
-	
-	p = Vote(prop_id=propid, vote_for=count )
-	return render_to_response('thanks_for_vote.html', {'ello': ello}
+	data = count[0]
+	upvote = data['vote_for']
+	new_upvote = upvote + 1
+	record = Vote.objects.get(prop_id=propid)
+	record.vote_for = new_upvote
+	record.save()
+	return render_to_response('thanks_for_vote.html')
+
+def down_vote(request, propid):
+
+	count = Vote.objects.filter(prop_id__contains=propid).values()
+	data = count[0]
+	downvote = data['vote_against']
+	new_downvote = downvote + 1
+	record = Vote.objects.get(prop_id=propid)
+	record.vote_against = new_downvote
+	record.save()
+	return render_to_response('thanks_for_vote.html')
+
+
 
 # def down_vote(request, propid):
-# 	# function
+
+
 # 	return render_to_response('thanks_for_vote.html', {'ello': ello}
 # 	
 # def view_vote_status(request, propid):
