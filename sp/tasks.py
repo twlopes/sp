04c634e5188f120aae59438d557054d1db16616f
Pat <patrickjamesbrown@gmail.com>
2012-07-17 23:44:33 +1000

    Have broker and queue adding prop when necessary.

--- sp/tasks.py
+++ sp/tasks.py
@@ -2,24 +2,46 @@ from celery.task import task
 from datetime import datetime, timedelta
 from celery.task import task
 from sp.props.models import Props
+from sp.microcons.models import MicroCons
 from django.db import models
 from django.forms import ModelForm
+from sp.props.diff_match_patch import *
 
 # function to save to database
 
 @task
 def expiry(z):
+	
+	# Get prop record
 	record = Props.objects.get(id=z)
+
+	# Get article number
+	article_number = record.microcons_id
 	
 	# Save into object instance.
-	
 	record.currency = "expired"
 	
 	# Send back into database.
-
 	record.save()
 	
-	# return z + y
+	# Get article record
+	article = MicroCons.objects.get(id=article_number)
+
+	if record.pass_status == "pass":
+		dfunction = diff_match_patch()
+		patch = record.patch
+		content = article.articlecontent
+		result = dfunction.patch_apply(patch, content)
+		
+		article.articlecontent = result
+
+		article.save()
+
+	else:
+		pass
+
+
+
 
 
 @task
