cf3fafaf8e901bacbba1a482aa0809785f2df653
Pat <patrickjamesbrown@gmail.com>
2012-08-01 19:34:19 +1000

    Delete text file.

--- sp/text.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-
-a = """“We are returning again to dominate territories we used to dominate, as well as more,” Mr. Baghdadi said in the Qaeda statement. He depicted the attacks as part of a battle launched by Sunnis against the country’s Shia leaders."""
-
-b = """territories we used to dominate, as well as more,” Mr. Baghdadi said in the Qaeda statement. He depicted the attacks as part of a battle country’s Shia leaders."""
