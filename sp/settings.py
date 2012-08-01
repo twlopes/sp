7f2ca3ca048863f41f29a923eed133363adf1471
Pat <patrickjamesbrown@gmail.com>
2012-08-02 09:40:07 +1000

    Bugfix.

--- sp/settings.py
+++ sp/settings.py
@@ -29,7 +29,7 @@ ADMINS = (
 #     }
 # }
 
-Code for heroku database
+# Code for heroku database
 
 import dj_database_url
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
